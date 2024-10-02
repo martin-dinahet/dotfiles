f37353449d14e648b101a52f134790ed481a937f
Martin Dinahet <martin.dinahet@proton.me>
2024-10-02 13:44:14 +0200

    added node_modules ignore for file picker

--- .config/nvim/lua/punpun/plugins/telescope.lua
+++ .config/nvim/lua/punpun/plugins/telescope.lua
@@ -1,16 +1,21 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  event = "VeryLazy",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    pickers = {
-      find_files = {
-        theme = "dropdown",
-        previewer = false,
-      },
-    },
-  },
+	"nvim-telescope/telescope.nvim",
+	event = "VeryLazy",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		defaults = {
+			file_ignore_patterns = {
+				"node_modules",
+			},
+		},
+		pickers = {
+			find_files = {
+				theme = "dropdown",
+				previewer = false,
+			},
+		},
+	},
 }
